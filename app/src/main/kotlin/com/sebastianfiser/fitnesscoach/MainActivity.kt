af2a760205e04aeb47ff9844562bc9bf4c3b929e
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-04 18:30:11 +0000

    main activity setup

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -1,1 +1,15 @@
-//main activity work in progress
+package com.sebastianfiser.fitnesscoach
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text("Hello app")
+        }
+    }
+}
