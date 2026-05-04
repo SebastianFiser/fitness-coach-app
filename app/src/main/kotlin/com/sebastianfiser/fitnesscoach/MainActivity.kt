c24af2ba7cda2a4613f7b72ad92b3f5b281600ca
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-04 19:57:51 +0000

    build fix

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sebastianfiser.fitnesscoach
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
+import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
