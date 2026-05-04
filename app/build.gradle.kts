ccde5fdccac77c5913cf29f90d9cd9c3862bd16c
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-04 19:45:00 +0000

    Source compatibility issues patch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,15 @@ android {
     namespace = "com.sebastianfiser.fitnesscoach"
     compileSdk = 34
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.sebastianfiser.fitnesscoach"
         minSdk = 26
