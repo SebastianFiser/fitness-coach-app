593d1cb21566154efa80bb10932cd4939448ac3a
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-04 19:25:22 +0000

    Add repositories to build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,11 @@ android {
     }
 }
 
+repositories {
+    google()
+    mavenCentral()
+}
+
 dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.material3:material3:1.2.0")
