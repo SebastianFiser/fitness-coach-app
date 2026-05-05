3b83c606c9d9dcf33416512492dfa418066ec797
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-05 11:56:26 +0200

    Update build.gradle.kts dependence change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ repositories {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.runtime:runtime:1.5.0")
 }
