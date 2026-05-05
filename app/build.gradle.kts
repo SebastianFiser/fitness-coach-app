382f9453878966e2d5ffabb0eb946ac43006e822
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-05 13:54:11 +0000

    typo fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrais.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 kotlin {
