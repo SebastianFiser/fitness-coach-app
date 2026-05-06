5e1db443ab402ed388757cf672ee93b3b6f72eac
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-06 10:41:13 +0000

    Appstart look update

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -3,16 +3,33 @@ package com.sebastianfiser.fitnesscoach
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
-import androidx.activity.compose.setContent
+import androidx.activity.compose.
+import androidx.activity.compose.WindowCompat
+import androidx.xompose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-           androidx.compose.material3.MaterialTheme {
-              androidx.compose.material3.Text("FitnessCoach funguje!")
-             }
+           StartContent()
          }
+    }
+
+    @Composable
+    fun StartContent() {
+        Box (
+            modifier = Modifier
+            .fillMaxSize
+            .background(Color.Gray)
+            
+        ) {
+            
+        }
     }
 }
