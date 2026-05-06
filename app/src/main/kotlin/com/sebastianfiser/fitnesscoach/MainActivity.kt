d8b03bca3c5b3fe2d07ecb0c3c6299b6e6ffb879
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-05-06 18:43:12 +0200

    impor name fix

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.activity.compose.setContent
-import androidx.activity.compose.WindowCompat
+import androidx.core.view.WindowCompat
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
