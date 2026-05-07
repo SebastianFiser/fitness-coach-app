649d69684788eb00b806f27d9d8cc42163ef8f85
Sebastian Fišer <119879744+SebastianFiser@users.noreply.github.com>
2026-05-07 13:10:15 +0200

    Update MainActivity.kt navBar colors change

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -51,7 +51,10 @@ fun StartContent() {
 
 @Composable
 fun BottomNav(modifier: Modifier = Modifier) {
-    NavigationBar(modifier = modifier) {
+    NavigationBar(
+       modifier = modifier
+       containerColor = Color.Black
+) {
         NavigationBarItem(
             icon = { Icon(Icons.Default.Home, contentDescription = null) },
             label = { Text("Overview") },
