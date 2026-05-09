f76ab9c4d88936b9d13590c239ccbab77c4b16f7
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-05-09 14:36:23 +0200

    fiix container color

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -286,6 +286,8 @@ fun displayDaySchedule(exercise: List<Exercise> = listOf(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Gray),
+        shape = RoundedCornerShape(28.dp)
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
