fa3be0029b26b2e18a34df237cfeb8dd9d464411
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-05-17 12:40:21 +0200

    fix missing imports

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/screens/OverviewScreen.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/screens/OverviewScreen.kt
@@ -1,6 +1,8 @@
 package com.sebastianfiser.fitnesscoach.screens
 
 import com.sebastianfiser.fitnesscoach.models.Exercise
+import com.sebastianfiser.fitnesscoach.models.Day
+import com.sebastianfiser.fitnesscoach.models.weekData
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
