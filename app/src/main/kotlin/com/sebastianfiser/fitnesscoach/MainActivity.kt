d3dc205d1ee579a6a08cc9748ba99e3620585399
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-06-03 22:03:53 +0200

    FIX WRONG SYNTAX

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/MainActivity.kt
@@ -89,7 +89,7 @@ fun StartContent(viewModel: AppViewModel) {
     Scaffold(
         bottomBar = {
             val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
-            if (loggedIn == true && currentRoute != Screen.Workout.route && currentRoute != Screen.Login.route && currentRoute != Screen.Register.route &&) {
+            if (loggedIn == true && currentRoute != Screen.Workout.route && currentRoute != Screen.Login.route && currentRoute != Screen.Register.route) {
                 BottomNav(navController = navController)
             }
         }
