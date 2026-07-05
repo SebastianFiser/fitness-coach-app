d0308c4b06810e2598cb91d9d6b0dac4b21043b5
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-07-05 12:59:01 +0200

    fix ?

--- app/src/main/kotlin/com/sebastianfiser/fitnesscoach/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/sebastianfiser/fitnesscoach/ui/theme/Theme.kt
@@ -2,6 +2,10 @@ package com.sebastianfiser.fitnesscoach.ui.theme
 
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import com.sebastianfiser.fitnesscoach.models.AppViewModel
 
 
 val DarkColors = darkColorScheme(
@@ -23,7 +27,7 @@ val LightColors = lightColorScheme(
 )
 
 @Composable
-fun FitCoachTheme(content: @Composable () -> Unit, viewModel: AppViewModel) {
+fun FitCoachTheme(viewModel: AppViewModel, content: @Composable () -> Unit) {
     val isDarkTheme = viewModel.isDarkTheme.value
     val colors = when (isDarkTheme) {
         true -> DarkColors
