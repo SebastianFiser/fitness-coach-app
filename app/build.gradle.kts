35b5a848d098d6d3bd77cfaabb7a3605242b4f69
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-07-12 21:30:38 +0200

    disable minifiing trigger __release__

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,10 @@ android {
         create("release") {
             val localProps = Properties()
             localProps.load(file("../local.properities").inputStream())
-            storeFile = file(localProps["KEYSTORE_PATH"] as String)
-            storePassword = localProps["KEYSTORE_PASSWORD"] as String
-            keyAlias = localProps["KEY_ALIAS"] as String
-            keyPassword = localProps["KEY_PASSWORD"] as String
+            storeFile = file(localProps["KEYSTORE_PATH"] as? String) ?: "dummy.keystore"
+            storePassword = (localProps["KEYSTORE_PASSWORD"] as? String) ?: ""
+            keyAlias = (localProps["KEY_ALIAS"] as? String) ?: ""
+            keyPassword = (localProps["KEY_PASSWORD"] as? String) ?: ""
         }
     }
 
@@ -52,7 +52,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
