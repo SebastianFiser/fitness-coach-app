2d4261b9ccca8e6c112827374ef8f106af88034c
SebastianFiser <sebastianfiser2275@gmail.com>
2026-08-06 13:10:52 +0200

    attempt #2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,5 +84,5 @@ dependencies {
     implementation("org.kimplify:countries-i18n:0.1.1")
     implementation("androidx.media3:media3-exoplayer:1.4.1")
     implementation("androidx.media3:media3-ui:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:2.3.0")
 }
