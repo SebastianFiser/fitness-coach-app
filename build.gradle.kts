8f5f2398432d7f96e009251ea6fa52afc33acc27
SebastianFiser <sebastianfiser2275@gmail.com>
2026-08-14 19:40:41 +0200

    fix versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.devtools.ksp") version "2.3.0-1.0.17" apply false
 }
