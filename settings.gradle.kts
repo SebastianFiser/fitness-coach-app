e55a7bd95acdcdb9d9fdfad40a9d2284fdb2427d
SebastianFiser <sebastianfiser2275@gmail.com>
2026-08-15 20:35:04 +0200

    fix build workflow __release__

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,11 @@ pluginManagement {
 }
 rootProject.name = "fitness-coach-app"
 include(":app")
-includeBuild("libs/KCountries") {
-    dependencySubstitution {
-        substitute(module("com.github.kimplify:countries-core")).using(project(":countries-core"))
-        substitute(module("com.github.kimplify:countries-i18n")).using(project(":countries-i18n"))
+val kCountriesDir = "libs/KCountries"
+if (kCountriesDir.exists())
+    includeBuild("libs/KCountries") {
+        dependencySubstitution {
+            substitute(module("com.github.kimplify:countries-core")).using(project(":countries-core"))
+            substitute(module("com.github.kimplify:countries-i18n")).using(project(":countries-i18n"))
+        }
     }
-}
