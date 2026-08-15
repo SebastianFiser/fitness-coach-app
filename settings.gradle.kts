e237ccea03ca22f78e054cc97bd67532caaefd30
SebastianFiser <sebastianfiser2275@gmail.com>
2026-08-15 20:36:27 +0200

    remove the check __release__

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,9 @@ pluginManagement {
 }
 rootProject.name = "fitness-coach-app"
 include(":app")
-val kCountriesDir = "libs/KCountries"
-if (kCountriesDir.exists())
-    includeBuild("libs/KCountries") {
-        dependencySubstitution {
-            substitute(module("com.github.kimplify:countries-core")).using(project(":countries-core"))
-            substitute(module("com.github.kimplify:countries-i18n")).using(project(":countries-i18n"))
-        }
+includeBuild("libs/KCountries") {
+    dependencySubstitution {
+        substitute(module("com.github.kimplify:countries-core")).using(project(":countries-core"))
+        substitute(module("com.github.kimplify:countries-i18n")).using(project(":countries-i18n"))
     }
+}
